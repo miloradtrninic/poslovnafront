163a49e720dcdc66ded7ca5c5811509ec478f47e
Sara Corak <saracorak0210@gmail.com>
2018-07-09 23:07:09 +0200

    Dodat servis za banku

---
+++ poslovnafront/src/app/model/bank.model.ts
@@ -0,0 +1,5 @@
+export class Bank {
+  constructor(public PIB: string, public naziv: string, public adresa: string, public email: string,
+              public web: string, public telefon: string, public fax: string, public sifraBanke: number,
+              public swift: string, public racunBrojRacuna: string) {}
+}
--- poslovnafront/src/app/services/entity-service.service.ts
+++ poslovnafront/src/app/services/entity-service.service.ts
@@ -1,31 +1,32 @@
-import { HttpClient, HttpRequest, HttpResponse} from '@angular/common/http';
+import {HttpClient, HttpParams, HttpRequest, HttpResponse} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import {Page} from '../model/page.model';
 
 
 export abstract class AbstractService<Entity, Key> {
-  actionUrl = 'http://localhost:8080';
-
-  constructor(protected http: HttpClient, protected url: string) {
+  actionUrl = 'http://localhost:8080/api';
+  protected constructor(protected http: HttpClient, protected url: string) {
     this.actionUrl = this.actionUrl + url;
   }
-  getAll(): Observable<any[]> {
-    return this.http.get<any[]>(this.actionUrl);
+  getAll(page: number, pageSize: number): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('page', page.toString());
+    params = params.append('size', pageSize.toString());
+    return this.http.get<Page<Entity>>(this.actionUrl + '/all', {params: params});
   }
-
-  getAllByZone(zoneId: number): Observable<Entity[]> {
-    return this.http.get<Entity[]>(this.actionUrl + '/getall/' + zoneId);
-  }
-
   getOne(id: Key): Observable<any> {
-    return this.http.get<any>(`${this.actionUrl}/${id}`);
+    return this.http.get<Entity>(`${this.actionUrl}/${id}`);
   }
-
   insert(toInsert: any): Observable<Entity> {
-    return this.http.post<Entity>(this.actionUrl + '/new', toInsert);
+    return this.http.post<Entity>(this.actionUrl + '/insert', toInsert,);
   }
-
+  delete(id: Key): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('id', id.toString());
+    return this.http.delete<Entity>(this.actionUrl + '/delete');
+  }
   update(toUpdate: any): Observable<any> {
-    return this.http.put<any>(this.actionUrl + '/update/', toUpdate);
+    return this.http.put(this.actionUrl + '/update/', toUpdate);
   }
 
 }
