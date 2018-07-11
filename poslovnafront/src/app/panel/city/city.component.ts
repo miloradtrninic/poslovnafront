7f48fef4fe5a5758749b0496d2f622a9665b2d1a
Sara Corak <saracorak0210@gmail.com>
2018-07-12 01:06:56 +0200

    Vracen izgubljeni komit

--- poslovnafront/src/app/panel/city/city.component.ts
+++ poslovnafront/src/app/panel/city/city.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, Output, EventEmitter } from '@angular/core';
+import { Pagination } from '../../pagination';
+import { CityModel } from '../../model/city.model';
+import { NgForm } from '@angular/forms';
+import { CityService } from '../../services/city.service';
+ 
+ @Component({
+   selector: 'app-city',
+   templateUrl: './city.component.html',
+   styleUrls: ['./city.component.css']
+ })
 
-@Component({
-  selector: 'app-city',
-  templateUrl: './city.component.html',
-  styleUrls: ['./city.component.css']
-})
-export class CityComponent implements OnInit {
+export class CityComponent extends Pagination<CityModel, string> implements OnInit {
 
-  constructor() { }
+  naziv = '';
+  message: string;
+  selected: CityModel;
+  @ViewChild('addForm') form: NgForm;
+  @ViewChild('editForm') eForm: NgForm;
+  @Output() addedCity: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit() {
+  constructor(public cityService: CityService ) {
+    super(cityService);  
+ } 
+ 
+   ngOnInit() {
+    this.search(0);
   }
 
-}
+  addCountry() {
+    const dir: CityModel = new CityModel(0, this.form.value['naziv']);
+    this.cityService.insert(dir).subscribe(
+      resp => {
+        this.pageset.content.push(resp);
+        this.addedCity.emit(resp);
+      }, error => {
+        this.message = JSON.stringify(error);
+      }
+    );
+  }
+
+  fillField(selected) {
+    this.eForm.controls['naziv'].setValue(selected.naziv);
+    this.selected = selected;
+  }
+
+  editCity() {
+    this.selected.naziv = this.eForm.controls['naziv'].value;
+    this.cityService.update(this.selected).subscribe(
+      resp => {
+        console.log(resp);
+      }, error => {
+        this.message = JSON.stringify(error);
+      }
+    );
+   }
+ }
--- poslovnafront/src/app/panel/country/country.component.ts
+++ poslovnafront/src/app/panel/country/country.component.ts
@@ -2,31 +2,34 @@ import { Component, OnInit, ViewChild, Output,EventEmitter } from '@angular/core
 import { NgForm } from '@angular/forms';
 import { CountryModel } from '../../model/country.model';
 import { CountryService } from '../../services/country.service';
-
+import { Pagination } from '../../pagination';
 
 @Component({
   selector: 'app-country',
   templateUrl: './country.component.html',
   styleUrls: ['./country.component.css']
 })
-export class CountryComponent implements OnInit {
-  name = '';
+export class CountryComponent extends Pagination<CountryModel, string> implements OnInit {
+  nazivDrzave = '';
   message: string;
+  selected: CountryModel;
   @ViewChild('addForm') form: NgForm;
   @ViewChild('editForm') eForm: NgForm;
   @Output() addedCountry: EventEmitter<any> = new EventEmitter();
-  constructor(public countryService: CountryService) { }
+  constructor(public countryService: CountryService) {
+        super(countryService);    
+   }
 
   ngOnInit() {
-    console.log('ngOnInit country list');
+    this.search(0);
   
   }
 
   addCountry() {
-    const dir: CountryModel = new CountryModel(0, this.form.value['name']);
-    console.log('aaaaaaaaaaaaaaaaaaaaaaa');
+    const dir: CountryModel = new CountryModel(0, this.form.value['nazivDrzave']);
     this.countryService.insert(dir).subscribe(
       resp => {
+        this.pageset.content.push(resp);
         this.addedCountry.emit(resp);
      //   this.countries = resp;
       }, error => {
@@ -34,4 +37,20 @@ export class CountryComponent implements OnInit {
       }
     );
   }
+
+  fillField(selected) {
+        this.eForm.controls['nazivDrzave'].setValue(selected.nazivDrzave);
+        this.selected = selected;
+      }
+    
+      editCountry() {
+        this.selected.nazivDrzave = this.eForm.controls['nazivDrzave'].value;
+        this.countryService.update(this.selected).subscribe(
+          resp => {
+            console.log(resp);
+          }, error => {
+            this.message = JSON.stringify(error);
+          }
+        );
+      }
 }
