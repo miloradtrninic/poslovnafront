bd51d82a9c90b7b1e3a611f1fb05a3d769586ea1
Arsenije Karpic <arsenije.karpic@gmail.com>
2018-07-13 23:49:03 +0200

    Dodavanje banke

--- poslovnafront/src/app/panel/bank/bank.component.ts
+++ poslovnafront/src/app/panel/bank/bank.component.ts
@@ -1,8 +1,10 @@
+import { BankCreation } from './../../model/bank.creation';
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Bank} from '../../model/bank.model';
 import {Pagination} from '../../pagination';
 import {BankService} from '../../services/bank.service';
 import {NgForm} from '@angular/forms';
+import { ToastrService } from '../../../../node_modules/ngx-toastr';
 
 @Component({
   selector: 'app-bank',
@@ -12,7 +14,10 @@ import {NgForm} from '@angular/forms';
 export class BankComponent extends Pagination<Bank, string> implements OnInit {
   @ViewChild('f') form: NgForm;
   selectedBrRacuna: string;
-  constructor(public bankService: BankService) {
+  constructor(
+    public bankService: BankService,
+    private toastrService: ToastrService
+    ) {
     super(bankService);
   }
 
@@ -21,6 +26,28 @@ export class BankComponent extends Pagination<Bank, string> implements OnInit {
   }
   getActionUrl(): string {
     return this.bankService.actionUrl;
+  }
+
+  onNovaBankaSubmit(forma: NgForm){
+    const banka: BankCreation = new BankCreation(
+      forma.value.pib,
+      forma.value.naziv,
+      forma.value.adresa,
+      forma.value.email,
+      forma.value.web,
+      forma.value.telefon,
+      forma.value.fax,
+      forma.value.sifraBanke,
+      forma.value.swift
+    );
+    this.bankService.insert(banka).subscribe(
+      (data) => {
+        this.toastrService.success('Uspesno dodana banka!');
+      },
+      (event) => {
+        this.toastrService.error(event.error);
+      }
+    );
   }
   downloadXml() {
     this.bankService.downloadXml(this.selectedBrRacuna,
